70a47c679a18898137c7d8ad63a61b344d8ced0f
kxh4892636 <kxh4892636@gmail.com>
2023-04-15 15:16:28 +0800

    merger

---
+++ server/src/utils/tools/child_process_promise.ts
@@ -0,0 +1,23 @@
+async function spawnChild() {
+  const { spawn } = require("child_process");
+  const child = spawn("node", ["child.js"]);
+
+  let data = "";
+  for await (const chunk of child.stdout) {
+    console.log("stdout chunk: " + chunk);
+    data += chunk;
+  }
+  let error = "";
+  for await (const chunk of child.stderr) {
+    console.error("stderr chunk: " + chunk);
+    error += chunk;
+  }
+  const exitCode = await new Promise((resolve, reject) => {
+    child.on("close", resolve);
+  });
+
+  if (exitCode) {
+    throw new Error(`subprocess error exit ${exitCode}, ${error}`);
+  }
+  return data;
+}
